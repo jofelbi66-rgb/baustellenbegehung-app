e4762e502476d2534b423d40d0e707a0d436ba9c
Joachim Jansen <jofelbi66@gmail.com>
2025-10-18 14:07:49 +0200

    Add emailjs configuration and logo URL

---
+++ src/Baustellenbegehung.jsx
@@ -0,0 +1,6 @@
+const EMAILJS_CONFIG = {
+  PUBLIC_KEY: "dein-public-key",
+  SERVICE_ID: "service_f2lezug",
+  TEMPLATE_ID: "template_3q4kf4r",
+};
+const LOGO_URL = "https://raw.githubusercontent.com/…/felbermayr_logo.png";
