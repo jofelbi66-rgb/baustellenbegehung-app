e105abb946a97525b03db7ff850d641c1fb006c7
Joachim Jansen <jofelbi66@gmail.com>
2026-01-17 07:57:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // WICHTIG für GitHub Pages Project Pages:
+  // Repo heißt: baustellenbegehung-app
   base: "/baustellenbegehung-app/",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true
-  }
 });
-
